fd74840c48fdddc38de9fa283e6bf90377255076
Samuel R <ronicksamuel@gmail.com>
2025-09-14 04:23:21 +0100

    gem:3742
    aded more stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,8 @@ import { userRoute } from './routes/users.route';
 import { WhatSappRoute } from './routes/whatsapp-settings.route';
 import { zoneRoute } from './routes/zone.route';
 import { branchRoute } from './routes/branch.route';
+import { areaRoute } from './routes/area.route';
+import { productRoute } from './routes/product.route';
 
 const app = express();
 app.use(express.json());
@@ -16,7 +18,9 @@ app.use('/api/user', userRoute);
 app.use('/api/organization', organizationRoute);
 app.use('/api/whatsapp-settings', WhatSappRoute);
 app.use('/api/organization/zone', zoneRoute);
+app.use('/api/organization/area', areaRoute);
 app.use('/api/organization/branch', branchRoute);
+app.use('/api/organization/product', productRoute);
 
 const PORT = appConfig.port;
 app.listen(PORT, async () => {
